80a9968d1c5d9bf0def17d9137226c399704cb14
Danny <danny20030509@gmail.com>
2023-02-10 23:12:00 +0800

    v0.2

--- index.js
+++ index.js
@@ -4,11 +4,13 @@ $(document).ready(function () {
         scrollHorizontally: true,
         slidesNavigation: true,
         scrollOverflow: false,
-        anchors: ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
+        anchors: ['enter_page', 'home_page', 'team_intro-1', 'team_intro-2', 'project-1', 'project-2', 'project-3', 'project-4', 'UAV-log', 'activities', 'support_us-1', 'support_us-2'],
         menu: '#gradient'
     });
     self.setInterval(function () {
-        fullpage_api.moveSlideRight();
+        if (window.location.href.split('#')[1].split('/')[0] == 'enter_page') {
+            fullpage_api.moveSlideRight();
+        }
     }, 9000);
     $('#home').click(function () {
         fullpage_api.moveSectionDown();
@@ -19,9 +21,17 @@ $(document).ready(function () {
     $('#arrow_up').click(function () {
         fullpage_api.moveSectionUp();
     });
+    $('.carousel').carousel({
+        interval: false,
+    });
 });
 
 function init() {
+    $('.info').html("");
+    $('.info').css({
+        opacity: 0,
+        transition: 'opacity 2s'
+    });
     $('#gradient').css({
         height: '100vh',
         transform: 'translateY(100vh)',
@@ -77,24 +87,36 @@ function pg3() {
     });
 }
 
+function pgf() {
+    $('#gradient').css({
+        height: '100vh',
+        transition: 'height 2s'
+    });
+    $('.info').html('<div class="row"><div class="col-3 offset-2" ><img src="./img/logo_circle.png"></div><div class="col-4 offset-1"><div class="text-center" id="slogan">航向天空不能沒有您!<br>支持&贊助我們!</div><div id="link"><img src="./img/mail_icon.png"><a href="mailto:nycuuav@gmail.com">nycuuav@gmail.com</a></div><div id="link"><img src="./img/fb_icon.png"><a href="https://www.facebook.com/NYCUUAVTeam">NYCU UAV</a></div><div id="link"><img src="./img/ig_icon.png"><a href="https://www.instagram.com/nycu_uav/">nycu_uav</a></div></div></div><div class="row text-center" id="address"><img class="col-1 offset-1" src="./img/address_icon.svg"><a href="https://goo.gl/maps/fpL6BARQz45qnjKW6" class="col-9">Address:新竹市東區大學路1001號國立陽明交通大學工程五館331B室</a></div>');
+    $('.info').css({
+        opacity: 1,
+        transition: 'opacity 5s'
+    });
+}
+
 function nav(page) {
     $('#gradient nav p a img[src="./img/nav_active.png"]').attr('src', './img/nav.png');
-    $('#gradient nav p[class="act"]').removeClass('act');
-    if (page <= 4) {
-        $('#gradient nav p[data-menuanchor="3"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="3"] a img').attr('src', './img/nav_active.png');
-    } else if (page <= 9) {
-        $('#gradient nav p[data-menuanchor="5"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="5"] a img').attr('src', './img/nav_active.png');
-    } else if (page <= 10) {
-        $('#gradient nav p[data-menuanchor="10"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="10"] a img').attr('src', './img/nav_active.png');
-    } else if (page <= 11) {
-        $('#gradient nav p[data-menuanchor="11"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="11"] a img').attr('src', './img/nav_active.png');
-    } else if (page <= 12) {
-        $('#gradient nav p[data-menuanchor="12"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="12"] a img').attr('src', './img/nav_active.png');
+    $('#gradient nav p[class~="act"]').removeClass('act');
+    if (page.split('-')[0] == 'team_intro') {
+        $('#gradient nav p[data-menuanchor="team_intro-1"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="team_intro-1"] a img').attr('src', './img/nav_active.png');
+    } else if (page.split('-')[0] == 'project') {
+        $('#gradient nav p[data-menuanchor="project-1"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="project-1"] a img').attr('src', './img/nav_active.png');
+    } else if (page.split('/')[0] == 'UAV-log') {
+        $('#gradient nav p[data-menuanchor="UAV-log"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="UAV-log"] a img').attr('src', './img/nav_active.png');
+    } else if (page == 'activities') {
+        $('#gradient nav p[data-menuanchor="activities"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="activities"] a img').attr('src', './img/nav_active.png');
+    } else if (page.split('-')[0] == 'support_us') {
+        $('#gradient nav p[data-menuanchor="support_us-1"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="support_us-1"] a img').attr('src', './img/nav_active.png');
     }
 }
 
@@ -111,25 +133,27 @@ function timeline(page) {
 
     $('#timeline a p').css('font-size', 'large');
 
-    $('#t20' + (page + 14).toString()).attr('src', './img/timeline_dot_active.png');
-    $('#t20' + (page + 14).toString()).css({
+    $('#t' + page.toString() + '+img').attr('src', './img/timeline_dot_active.png');
+    $('#t' + page.toString() + '+img').css({
         width: '3vw',
         transition: 'width 1s'
     });
 
-    $('#y20' + (page + 14).toString()).css('font-size', 'x-large');
+    $('#t' + page.toString()).css('font-size', 'x-large');
 }
 
 function main() {
     init();
-    page = window.location.href.split('#')[1] - '0';
-    if (page == 2) {
+    page = window.location.href.split('#')[1];
+    if (page == 'home_page') {
         pg2();
-    } else if (page >= 3) {
+    } else if (page.split('/')[0] != 'enter_page') {
         pg3();
         nav(page);
-        if (page >= 5 && page <= 9) {
-            timeline(page);
+        if (page.split('-')[0] == 'project') {
+            timeline(page.split('-')[1]);
+        } else if (page == 'support_us-2') {
+            pgf();
         }
     }
 }
