d0bd1f40b4395128a96db135e5a0604a3ab8df4b
Danny <danny20030509@gmail.com>
2023-02-12 15:09:57 +0800

    v0.3

--- index.js
+++ index.js
@@ -1,12 +1,38 @@
+const page_anchors = [['team_intro-1', 'team_intro-2'],
+['team_intro-1', 'team_intro-2'],
+['team_intro-1', 'team_intro-2'],
+['project-1', 'project-2', 'project-3', 'project-4'],
+['UAV_log'],
+['activities'],
+['awards'],
+['team_intro-1', 'team_intro-2'],];
+
+const page_hashtag = ['enter_page', 'home_page', 'team_intro', 'project', 'UAV_log', 'activities', 'awards', 'support_us'];
+
+const page_num = page_hashtag.indexOf(window.location.href.split('#')[1].split('/')[0].split('-')[0]);
+
 $(document).ready(function () {
+    for (i = page_anchors[page_num].length - 1; i >= 0; i--) {
+        $('#home').after('<div class="section" id="' + i + '">');
+        if (page_hashtag[page_num] == 'UAV_log') {
+            $('#' + i).addClass('log');
+        } else if (page_hashtag[page_num] == 'activities') {
+            $('#' + i).addClass('gallery');
+        }
+        $('#' + i).load(page_anchors[page_num][i] + '.html');
+    }
     $('#fullpage').fullpage({
         autoScrolling: true,
         scrollHorizontally: true,
         slidesNavigation: true,
         scrollOverflow: false,
-        anchors: ['enter_page', 'home_page', 'team_intro-1', 'team_intro-2', 'project-1', 'project-2', 'project-3', 'project-4', 'UAV-log', 'activities', 'support_us-1', 'support_us-2'],
+        anchors: ['enter_page', 'home_page', ...page_anchors[page_num], 'support_us'],
         menu: '#gradient'
     });
+    if (page_anchors[page_num].includes(window.location.href.split('#')[1].split('/')[0])) {
+        fullpage_api.silentMoveTo(3);
+        pg3_silent();
+    }
     self.setInterval(function () {
         if (window.location.href.split('#')[1].split('/')[0] == 'enter_page') {
             fullpage_api.moveSlideRight();
@@ -14,12 +40,6 @@ $(document).ready(function () {
     }, 9000);
     $('#home').click(function () {
         fullpage_api.moveSectionDown();
-    });
-    $('#arrow_down').click(function () {
-        fullpage_api.moveSectionDown();
-    });
-    $('#arrow_up').click(function () {
-        fullpage_api.moveSectionUp();
     });
     $('.carousel').carousel({
         interval: false,
@@ -70,7 +90,6 @@ function pg3() {
     $('#gradient').addClass('fixed-top');
     $('#gradient').css({
         height: '13vh',
-        background: 'linear-gradient(white, #0F2540)',
         transform: 'translateY(0)',
         opacity: 1,
         transition: 'height 2s, opacity 2s'
@@ -87,12 +106,45 @@ function pg3() {
     });
 }
 
+function pg3_silent() {
+    $('#gradient').addClass('fixed-top');
+    $('#gradient').css({
+        height: '13vh',
+        transform: 'translateY(0)',
+        opacity: 1,
+        transition: 'height 0s, opacity 0s'
+    });
+    $('#logo_circle').css({
+        width: '10vh',
+        margin: '1.5vh 1.5vh',
+        opacity: 1,
+        transition: 'width 0s, height 0s, margin 0s, opacity 0s'
+    });
+    $('#gradient nav p').not('#gradient nav p:first').css({
+        opacity: 1,
+        transition: 'opacity 0s'
+    });
+}
+
 function pgf() {
+    $('#gradient').addClass('fixed-top');
     $('#gradient').css({
         height: '100vh',
-        transition: 'height 2s'
+        transform: 'translateY(0)',
+        opacity: 1,
+        transition: 'height 2s, opacity 2s'
     });
-    $('.info').html('<div class="row"><div class="col-3 offset-2" ><img src="./img/logo_circle.png"></div><div class="col-4 offset-1"><div class="text-center" id="slogan">航向天空不能沒有您!<br>支持&贊助我們!</div><div id="link"><img src="./img/mail_icon.png"><a href="mailto:nycuuav@gmail.com">nycuuav@gmail.com</a></div><div id="link"><img src="./img/fb_icon.png"><a href="https://www.facebook.com/NYCUUAVTeam">NYCU UAV</a></div><div id="link"><img src="./img/ig_icon.png"><a href="https://www.instagram.com/nycu_uav/">nycu_uav</a></div></div></div><div class="row text-center" id="address"><img class="col-1 offset-1" src="./img/address_icon.svg"><a href="https://goo.gl/maps/fpL6BARQz45qnjKW6" class="col-9">Address:新竹市東區大學路1001號國立陽明交通大學工程五館331B室</a></div>');
+    $('#logo_circle').css({
+        width: '10vh',
+        margin: '1.5vh 1.5vh',
+        opacity: 1,
+        transition: 'width 2s, height 2s, margin 2s, opacity 2s'
+    });
+    $('#gradient nav p').not('#gradient nav p:first').css({
+        opacity: 1,
+        transition: 'opacity 3s'
+    });
+    $('.info').load('./info.html');
     $('.info').css({
         opacity: 1,
         transition: 'opacity 5s'
@@ -108,19 +160,19 @@ function nav(page) {
     } else if (page.split('-')[0] == 'project') {
         $('#gradient nav p[data-menuanchor="project-1"]').addClass('act');
         $('#gradient nav p[data-menuanchor="project-1"] a img').attr('src', './img/nav_active.png');
-    } else if (page.split('/')[0] == 'UAV-log') {
-        $('#gradient nav p[data-menuanchor="UAV-log"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="UAV-log"] a img').attr('src', './img/nav_active.png');
+    } else if (page == 'UAV_log') {
+        $('#gradient nav p[data-menuanchor="UAV_log"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="UAV_log"] a img').attr('src', './img/nav_active.png');
     } else if (page == 'activities') {
         $('#gradient nav p[data-menuanchor="activities"]').addClass('act');
         $('#gradient nav p[data-menuanchor="activities"] a img').attr('src', './img/nav_active.png');
-    } else if (page.split('-')[0] == 'support_us') {
-        $('#gradient nav p[data-menuanchor="support_us-1"]').addClass('act');
-        $('#gradient nav p[data-menuanchor="support_us-1"] a img').attr('src', './img/nav_active.png');
+    } else if (page == 'awards') {
+        $('#gradient nav p[data-menuanchor="awards"]').addClass('act');
+        $('#gradient nav p[data-menuanchor="awards"] a img').attr('src', './img/nav_active.png');
     }
 }
 
-function timeline(page) {
+function timeline(num) {
     $('#timeline').css({
         transform: 'translateY(0)',
         transition: 'transform 3s'
@@ -133,13 +185,17 @@ function timeline(page) {
 
     $('#timeline a p').css('font-size', 'large');
 
-    $('#t' + page.toString() + '+img').attr('src', './img/timeline_dot_active.png');
-    $('#t' + page.toString() + '+img').css({
+    $('#t' + num.toString() + '+img').attr('src', './img/timeline_dot_active.png');
+    $('#t' + num.toString() + '+img').css({
         width: '3vw',
         transition: 'width 1s'
     });
 
-    $('#t' + page.toString()).css('font-size', 'x-large');
+    $('#t' + num.toString()).css('font-size', 'x-large');
+}
+
+function contain(page) {
+
 }
 
 function main() {
@@ -147,13 +203,14 @@ function main() {
     page = window.location.href.split('#')[1];
     if (page == 'home_page') {
         pg2();
+    } else if (page == 'support_us') {
+        nav(page);
+        pgf();
     } else if (page.split('/')[0] != 'enter_page') {
         pg3();
         nav(page);
         if (page.split('-')[0] == 'project') {
             timeline(page.split('-')[1]);
-        } else if (page == 'support_us-2') {
-            pgf();
         }
     }
 }
@@ -163,5 +220,9 @@ $(window).on('load', function () {
 });
 
 $(window).on('hashchange', function () {
+    new_page = window.location.href.split('#')[1].split('/')[0];
+    if (!(['enter_page', 'home_page', ...page_anchors[page_num], 'support_us'].includes(new_page))) {
+        location.reload();
+    }
     main();
 });
